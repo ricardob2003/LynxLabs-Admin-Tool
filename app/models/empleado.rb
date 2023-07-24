62d1089ec5ef504db0172712d2e22c393704f780
Ricardo Barahona <ricardodbarahona@gmail.com>
2023-07-24 14:52:47 -0600

    Validations for Empleado Module, error messages not rendering

--- app/models/empleado.rb
+++ app/models/empleado.rb
@@ -3,4 +3,10 @@ class Empleado < ApplicationRecord
   has_many :proyectos, through: :project_assignments
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
+
+  # Validations for Empleado model
+  validates :nombre, :apellido, :nacionalidad, :developer, presence: { message: "no puede estar en blanco" }
+  validates :estado_civil, inclusion: { in: ["Soltero", "Casado", "Union Libre", "Divorciado", "Viudo"], message: "debe ser una opción válida" }
+  validates :dni, :rtn, presence: true, uniqueness: true
+  validates :contrato, inclusion: { in: ["Full-Time", "Part-Time", "Contratista", "Empleado No Activo"], message: "debe ser una opción válida" }
 end
